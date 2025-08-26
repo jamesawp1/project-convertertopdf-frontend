55ec00f936fc74df5c98d0bf143870bccaff6ee6
jamesawp1 <jamesawp@protonmail.com>
2025-08-26 15:41:00 -0300

    feat: Style the footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '../../../public/vite.svg'
 import './App.css'
+import Footer from "../Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="page">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import githubLogo from "../../images/github.png";
+import linkedinLogo from "../../images/linkedin.png";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">&copy; 2025 James Jr.</p>
+      <div className="footer__wrapper">
+        <a href="#" className="footer__link">
+          <p className="footer__home">Início</p>
+        </a>
+        <ul className="footer__list">
+        <li className="footer__list-item">
+          <a href="https://github.com/jamesawp1" className="footer__link">
+            <img src={githubLogo} alt="Logo do Github" className="footer__icon"/>
+          </a>
+        </li>
+        <li className="footer__list-item">
+          <a href="https://www.linkedin.com/in/james-roggia-damo" className="footer__link">
+            <img src={linkedinLogo} alt="Logo do Linkedin" className="footer__icon"/>
+          </a>
+        </li>
+      </ul>
+      </div>
+    </footer>
+  );
+}
