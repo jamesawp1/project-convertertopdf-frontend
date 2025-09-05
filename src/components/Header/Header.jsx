1aa51e7236715c79cf1170246ee554cae05b651e
jamesawp1 <jamesawp@protonmail.com>
2025-09-05 10:47:50 -0300

    style: Change header color font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from "../Header/Navigation/Navigation";
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">PDF Converter</h1>
+      <h1 className="header__title">News Explorer</h1>
       <Navigation/>
     </header>
   );
